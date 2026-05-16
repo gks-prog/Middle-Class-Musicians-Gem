698af6f4134e363d5654f84291c7f2e9e7619fe9
gks-prog <gauravkumar41917@gmail.com>
2026-05-16 08:05:49 +0530

    Create SmoothScroll.tsx

---
+++ src/components/layout/SmoothScroll.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ReactLenis } from '@studio-freight/react-lenis'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useEffect } from 'react'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Sync GSAP with Lenis
+    function update(time: number) {
+      gsap.ticker.tick(time * 1000)
+    }
+    gsap.ticker.add(update)
+    return () => gsap.ticker.remove(update)
+  }, [])
+
+  return (
+    <ReactLenis root options={{ lerp: 0.05, smoothWheel: true }}>
+      {children}
+    </ReactLenis>
+  )
+}
